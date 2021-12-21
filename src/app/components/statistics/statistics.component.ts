75c50190c7fdc28ac790aa618932756227952dad
Dragon Flip <62544653+dragonflip@users.noreply.github.com>
2021-12-21 23:13:18 +0200

    Added income/outcome pie charts

--- src/app/components/statistics/statistics.component.ts
+++ src/app/components/statistics/statistics.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ChartOptions, ChartData } from 'chart.js';
 
+interface ChartObject {
+  data: ChartData;
+  options: ChartOptions;
+  show: boolean;
+}
+
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
@@ -12,51 +18,148 @@ export class StatisticsComponent implements OnInit {
   summary: any = [];
   dateRange: string = '1 month';
 
-  chartData: ChartData = {
-    labels: ['Доходи', 'Витрати'],
-    datasets: [
-      {
-        data: [],
-        backgroundColor: ['rgb(105, 240, 174)', 'rgb(244, 67, 54)'],
-        hoverBackgroundColor: ['rgb(105, 240, 174)', 'rgb(244, 67, 54)'],
-        hoverBorderColor: '#000',
+  // Summary chart
+  summaryChart: ChartObject = {
+    data: {
+      labels: ['Доходи, грн.', 'Витрати, грн.'],
+      datasets: [
+        {
+          data: [],
+          backgroundColor: ['rgb(105, 240, 174)', 'rgb(244, 67, 54)'],
+          hoverBackgroundColor: ['rgb(105, 240, 174)', 'rgb(244, 67, 54)'],
+          hoverBorderColor: '#000',
+        },
+      ],
+    },
+    options: {
+      color: '#fff',
+      plugins: {
+        title: {
+          display: true,
+          text: 'Відношення доходів до витрат',
+          color: '#fff',
+        },
       },
-    ],
+    },
+    show: false,
+  };
+
+  // Categories income chart
+  categoriesIncomeChart: ChartObject = {
+    data: {
+      labels: [],
+      datasets: [
+        {
+          data: [],
+          hoverBorderColor: '#000',
+        },
+      ],
+    },
+    options: {
+      color: '#fff',
+      plugins: {
+        title: {
+          display: true,
+          text: 'Доходи по категоріях',
+          color: '#fff',
+        },
+      },
+    },
+    show: false,
   };
   chartOptions: ChartOptions = {
     color: '#fff',
+
+  // Categories outcome chart
+  categoriesOutcomeChart: ChartObject = {
+    data: {
+      labels: [],
+      datasets: [
+        {
+          data: [],
+          hoverBorderColor: '#000',
+        },
+      ],
+    },
+    options: {
+      color: '#fff',
+      plugins: {
+        title: {
+          display: true,
+          text: 'Витрати по категоріях',
+          color: '#fff',
+        },
+      },
+    },
+    show: false,
+  };
   };
 
   constructor(private http: HttpClient) {}
 
   // Init
   ngOnInit(): void {
-    console.log('INIT');
-
     this.getData();
   }
 
   // Get data
   getData(): void {
-    console.log('GET DATA');
-
-    this.http.get(`/categories/${this.dateRange}`).subscribe((res) => {
-      this.categories = res;
-    });
-
+    // Get summary
     this.http.get(`/summary/${this.dateRange}`).subscribe((res) => {
       this.summary = res;
 
-      this.chartData.datasets[0].data = [
+      this.summaryChart.data.datasets[0].data = [
         this.summary.income,
         this.summary.outcome,
       ];
+
+      this.summaryChart.show = true;
+    });
+
+    // Get categories
+    this.http.get(`/categories/${this.dateRange}`).subscribe((res) => {
+      this.categories = res;
+
+      // Income
+      const categoriesIncome = this.categories.filter(
+        (category: any) => category.income > 0
+      );
+
+      // Labels
+      this.categoriesIncomeChart.data.labels = categoriesIncome.map(
+        (category: any) => category.name
+      );
+      // Dataset
+      this.categoriesIncomeChart.data.datasets[0].data = categoriesIncome.map(
+        (category: any) => category.income
+      );
+
+      // Outcome
+      const categoriesOutcome = this.categories.filter(
+        (category: any) => category.outcome < 0
+      );
+
+      // Labels
+      this.categoriesOutcomeChart.data.labels = categoriesOutcome.map(
+        (category: any) => category.name
+      );
+      // Dataset
+      this.categoriesOutcomeChart.data.datasets[0].data = categoriesOutcome.map(
+        (category: any) => category.outcome
+      );
+
+      this.categoriesIncomeChart.show = true;
+      this.categoriesOutcomeChart.show = true;
     });
   }
 
   // Set date range
   setDateRange(): void {
-    console.log('SET DATE');
+    this.summaryChart.show = false;
+    this.categoriesIncomeChart.show = false;
+    this.categoriesOutcomeChart.show = false;
+
+    // this.incomeOutcomeChart.show = false;
 
     this.getData();
   }
